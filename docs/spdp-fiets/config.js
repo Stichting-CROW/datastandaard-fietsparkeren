2acfa38c119e581b1ccfc6ef3a6a81059575ee51
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2022-08-03 08:48:41 +0200

    fix: spdp-fiets, api-fiets documenten restore

---
+++ docs/spdp-fiets/config.js
@@ -0,0 +1,28 @@
+var respecConfig = {
+  specType: "API",
+  specStatus: "RESC",
+  date: "2022-01-01",
+  isPreview: true,
+  edDraftURI:
+    "https://docs.google.com/document/d/1SANKIH4Gam9CH1N8NPgtB1efg2LEEWSmGNnR8AbH2zU/edit?usp=sharing",
+  github: "https://github.com/Stichting-CROW/datastandaard-fietsparkeren",
+  pubDomain: "datastandaard-fietsparkeren",
+  shortName: "spdp-fiets",
+  lint: {
+    "no-http-props": false,
+  },
+  editors: [
+    {
+      name: "Hans Versluis",
+      company: "Haverweb",
+    },
+    {
+      name: "Redmer Kronemeijer",
+      company: "CROW",
+    },
+    {
+      name: "Ruben Bino",
+      company: "Trajan",
+    },
+  ],
+};
